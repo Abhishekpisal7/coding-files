b88d0acbfa514b043c4745feadff3f046233e1c6
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-07-26 21:59:46 +0530

    mani file

--- main.tf
+++ main.tf
@@ -1,59 +1,51 @@
 provider "aws" {
- 
-    region = "ap-south-1"
+  region = "ap-south-1"
+}
+
+variable "ami_id" {
+    description = "value of ami-id"
+    type = string
+    default = "ami-0d0ad8bb301edb745"
   
 }
 
-variable "ami-id" {
-    description = "The AMI ID to use for the instance"
-    type        = string
-    default     = "ami-0f918f7e67a3323f0"  # Example AMI ID, replace with your own
+variable "instance_type" {
+    description = "value od instance type"
+    type = string
+    default ="t2.micro"
   
 }
 
-variable "instance-value" {
-    description = "The type of instance to create"
-    type        = string
-    default     = "t2.micro"  # Example instance type, replace with your own
-  
+variable "key_name" {
+    description = "value of key name"
+    type = string
+    default = "newkey" 
 }
 
-variable "key" {
-    description = "The name of the key pair to use for SSH access"
-    type        = string
-    default     = "newkey"  # Example key pair name, replace with your own
-  
-}
+resource "aws_instance" "terraform-instance" {
 
-
-resource "aws_instance" "test_instance" {
-   
-    ami           = var.ami-id
-    instance_type = var.instance-value
-    key_name      = var.key
-    
-   
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name 
 
     tags = {
-        Name = "TestInstance"
+        Name = "Terraform-Instance"
         Environment = "DevOps"
         Owner = "Abhishek Pisal"
-        instance ="TestInstance-${count.index + 1}"
-    }  
-  
-}
-
-
- output "TestInstanceID" {
-
-    value = aws_instance.test_instance.id
-    description = "The ID of the test instance"
     }
 
-output "TestInstancePublicIP" {
-
-    value = aws_instance.test_instance.public_ip
-    description = "The public IP address of the test instance"
-    
 }
 
+output "instance-id" {
+
+    value = aws_instance.terraform-instance.id
+    description = "ID of the created terraform-instance"
+
+}
+
+output "public-ip" {
+
+    value =aws_instance.terraform-instance.public_ip
+    description = "Public IP of the created terraform-instance"
+
+}
