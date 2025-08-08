bba4bcb3443650a863de706ab717ac9ba3ac6c34
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-08-08 16:20:33 +0530

    new commit

--- new/hello.py
+++ new/hello.py
@@ -2,6 +2,6 @@ print("Hello, World!")
 
 print("My Names is Abhishek Pisal")
 
-print("Demo for jenkins to pull code")
+print("Demo for jenkins to pull code and execute")
 
 print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
