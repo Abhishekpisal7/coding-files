1f3548bc01d94c78c7f6286f5723f47ce9ced17e
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-08-09 00:19:50 +0530

    new

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+
+print("My Names is Abhishek Pisal")
+
+print("Demo for jenkins to pull code and execute")
+
+print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
