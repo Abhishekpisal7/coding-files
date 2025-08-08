a70fc766e7bf3270346662c5d3f7e395fd9820f5
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-08-08 16:10:34 +0530

    demo jenkins

--- new/hello.py
+++ new/hello.py
@@ -2,4 +2,6 @@ print("Hello, World!")
 
 print("My Names is Abhishek Pisal")
 
+print("Demo for jenkins")
+
 print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
