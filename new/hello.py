3c4c5cccd35ccf6ff9385bca949cdad41e0724c8
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-08-12 19:12:03 +0530

    Sytax corrected

--- new/hello.py
+++ new/hello.py
@@ -1,7 +1,7 @@
 print("Hello, World!")
 
-print("This Private Repo")
+print("This Private Repo.")
 
-print("Demo for jenkins to pull code and execute")
+print("Demo for jenkins to pull code and execute.")
 
 print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
