7cc6b758d92a744da3a7a4dd45b9b6b33935152b
Abhishekpisal7 <115809300+Abhishekpisal7@users.noreply.github.com>
2025-08-12 13:51:27 +0530

    Delete new directory

--- new/hello.py
+++
@@ -1,5 +0,0 @@
-print("Hello, World!")
-
-print("Demo for jenkins to pull code and execute")
-
-print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
