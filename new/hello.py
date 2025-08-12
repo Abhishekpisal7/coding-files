8d0d2415a9d876d6767af8abc19211a65f709991
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-08-12 16:07:03 +0530

    hello file

---
+++ new/hello.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+
+print("Demo for jenkins to pull code and execute")
+
+print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
