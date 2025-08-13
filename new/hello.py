d2fafda175336384c5cf45a8d6761bf0763c3c8d
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-08-13 18:26:48 +0530

    fgf

--- new/hello.py
+++ new/hello.py
@@ -13,5 +13,3 @@ print("This Private Repo.")
 print("Demo for jenkins to pull code and execute.")
 
 print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
-
-print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
