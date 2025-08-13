176543a3e35e10b4443b6c655993ecebfbf079ae
AbhishekPisal <Abhishekshivajipisal@gmail.com>
2025-08-13 18:14:45 +0530

    modified 17 added

--- new/hello.py
+++ new/hello.py
@@ -13,3 +13,5 @@ print("This Private Repo.")
 print("Demo for jenkins to pull code and execute.")
 
 print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
+
+print("This demo for jenkins when ever the chenges made in this file and pushed to github it will run the file.")
